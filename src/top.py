3cd7fcccef107ab09056414fc7426ecddfb7570d
Vijaya Krishna Kasula <krishna.vijay4444@gmail.com>
2019-06-14 08:37:53 +0530

    redesign to use parser and handle multi source nodes after block end including else less if ladders. Also support while loops

--- src/top.py
+++ src/top.py
@@ -7,6 +7,7 @@
 import fileinput
 import sly
 import pygraphviz as pgv
+import json
 import os
 import sys
 
@@ -14,19 +15,21 @@ import sys
 class FlowLexer(sly.Lexer):
     "Tokenize the pseudocode"
 
-    tokens = {IF, ELSE, COND, STATE, PARAN_OPEN, PARAN_CLOSE}
+    tokens = {IF, ELSE, ELSEIF, WHILE, COND, STATE, PARAN_OPEN, PARAN_CLOSE}
 
     ignore = ' \t'
     ignore_comment = r'\#.*'
 
+    ELSEIF = r'else if'
     IF = r'if'
     ELSE = r'else'
+    WHILE = r'while'
     COND = r'\(.+?\)'
     PARAN_OPEN = r'\{'
     PARAN_CLOSE = r'\}'
     STATE = r'.+;'
-    
-    
+
+
     # Line number tracking
     @_(r'\n+')
     def ignore_newline(self, t):
@@ -36,10 +39,89 @@ class FlowLexer(sly.Lexer):
     #    print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
     #    self.index += 1
 
-class FlowBuilder:
-    """Builds Flow chart"""
 
-    # TODO: This is better done as a parser
+class FlowParser(sly.Parser):
+    "Build AST"
+
+    tokens = FlowLexer.tokens
+    ast = ()
+
+    @_('block block')
+    def block(self, p):
+        if isinstance(p[1], tuple):
+            # Do not create too many nested tuples
+            return (p[0], *p[1])
+        else:
+            return (p[0], p[1])
+
+    @_('ifBlock')
+    def block(self, p):
+        # Just the if
+        return [p[0]]
+
+    @_('ifBlock elseIfBlock')
+    def block(self, p):
+        # Else less full ladder
+        return [p[0], p[1]]
+
+    @_('ifBlock elseBlock')
+    def block(self, p):
+        # Full ladder without elseif
+        return [p[0], p[1]]
+
+    @_('ifBlock elseIfBlock elseBlock')
+    def block(self, p):
+        # Full ladder
+        if isinstance(p[1], list):
+            return [p[0], *p[1], p[2]]
+        else:
+            return [p[0], p[1], p[2]]
+
+    @_('elseIfBlock elseIfBlock')
+    def elseIfBlock(self, p):
+        # Multiple Else Ifs
+        # Make it a list
+        p[0] = [p[0]]
+        if isinstance(p[1], list):
+            for x in p[1]:
+                p[0].append(x)
+        else:
+            p[0].append(p[1])
+
+        return p[0]
+
+    @_('ELSEIF cond PARAN_OPEN block PARAN_CLOSE')
+    def elseIfBlock(self, p):
+        # Single else if
+        return {'node':'elseif', 'cond':p[1], 'body':p[3]}
+
+    @_('IF cond PARAN_OPEN block PARAN_CLOSE')
+    def ifBlock(self, p):
+        # Single if
+        return {'node':'if', 'cond':p[1], 'body':p[3]}
+
+    @_('ELSE PARAN_OPEN block PARAN_CLOSE')
+    def elseBlock(self, p):
+        # Single else
+        return {'node':'else', 'body':p[2]}
+
+    @_('WHILE cond PARAN_OPEN block PARAN_CLOSE')
+    def block(self, p):
+        # While as a tuple
+        return {'node':'while', 'cond':p[1], 'body':p[3]}
+
+    @_('COND')
+    def cond(self, p):
+        # Return as a string, do not create more nested tuples
+        return p[0][1:-1]
+
+    @_('STATE')
+    def block(self, p):
+        return p[0][0:-1]
+
+
+class AstBuilder:
+    """Builds AST"""
 
     attr = {
         'STATE': {
@@ -53,80 +135,99 @@ class FlowBuilder:
             'color':'red'
         }
     }
-    
-    def __init__(self, tokens):
+
+    def __init__(self, elements):
         '''Initialize'''
         self.dot = pgv.AGraph(strict=True, directed=True, rankdir='TD')
-        self.tokens = tokens
-        self.stack = []
         # self.dot.layout()
-        self.generate()
 
-    def generate(self):
-        '''Generate the graph'''
-        for token in self.tokens:
-            self.handleToken(token)
-
-    def getLastToken(self, valuesToIgnore=None):
-        "Returns the last token whose value is not in the list of given values to ignore"
-
-        if valuesToIgnore == None:
-            valuesToIgnore = ['if', '{'] # , '}']
-        else:
-            valuesToIgnore.append('if')
-            valuesToIgnore.append('{')
-            # valuesToIgnore.append('}')
-        
-        for token in reversed(self.stack):
-            if token.value not in valuesToIgnore:
-                return token
-        
-        return None
-
-    def popUntil(self, value):
-        "Pops tokens until the given token value is found"
-
-        while self.stack[-1].value != value:
-            self.stack.pop()
-        
-        self.stack.pop()
+        lastElement = None
+        for element in elements:
+            lastElement = self.handle(element, lastElement)
 
 
-    def handleToken(self, token):
-        '''Handles one token at a time'''
+    def connectFrom(self, node, lastElement=None, firstEdgeName=None):
+        "Connect current element from all last elements"
 
-        # Get rid of unwanted characters from State and Condition
-        if token.type == 'STATE':
-            token.value = token.value[0:-1]
-        if token.type == 'COND':
-            token.value = token.value[1:-1]
-        
-        # When a closing } is found, pop all until opening { - and nothing much to do anyway
-        if len(self.stack) and (token.value == '}'):
-            self.popUntil('{')
+        if not lastElement:
             return
 
-        # State
-        if token.type in ['STATE', 'COND']:
-            # Add a node
-            self.dot.add_node(token.value, **self.attr[token.type])
+        for x in lastElement:
+            if firstEdgeName:
+                self.dot.add_edge(x, node, label=firstEdgeName)
+            else:
+                self.dot.add_edge(x, node)
 
-            # Add edge
-            if len(self.stack):
-                lastToken = self.getLastToken()
-                assert lastToken != None, "Probable syntax error"
+            firstEdgeName = None
 
-                if lastToken.type == 'ELSE':
-                    lastToken = self.getLastToken(['else'])
-                    self.dot.add_edge(lastToken.value, token.value, label='False')
-                elif lastToken.type == 'COND':
-                    self.dot.add_edge(lastToken.value, token.value, label='True')
+
+    def handle(self, element, lastElement=None, firstEdgeName=None):
+        '''Generate one node at a time'''
+
+        if isinstance(element, str):
+            self.dot.add_node(element, **self.attr['STATE'])
+            self.connectFrom(element, lastElement, firstEdgeName)
+            return [element]
+
+        # Handle complex nodes
+        if isinstance(element, list):
+            # list is if ladder
+            assert element[0]['node'] == 'if', "Expecting an IF here"
+
+            # Handle IF
+            self.dot.add_node(element[0]['cond'], **self.attr['COND'])
+            self.connectFrom(element[0]['cond'], lastElement, firstEdgeName)
+
+            # If else part is missing, retain the lastElement to return back
+            # When else part is present, lastElement is no more required
+            if element[-1]['node'] == 'else':
+                lastElement = []
+            # Set the last condition
+
+            lastCondition = element[0]['cond']
+
+            # Handle IF block of ladder
+            for x in self.handle(element[0]['body'], [element[0]['cond']], 'True'):
+                lastElement.append(x)
+
+            # Get rid of the IF part
+            element = element[1:]
+            for i in range(len(element)):
+                if 'cond' in element[i]:
+                    self.dot.add_node(element[i]['cond'], **self.attr['COND'])
+                    self.connectFrom(element[i]['cond'], [lastCondition], 'False')
+                    lastCondition = element[i]['cond']
+                    for x in self.handle(element[i]['body'], [lastCondition], 'True'):
+                        lastElement.append(x)
                 else:
-                    self.dot.add_edge(lastToken.value, token.value)
+                    for x in self.handle(element[i]['body'], [lastCondition], 'False'):
+                        lastElement.append(x)
 
-        # Put it on stack at last
-        self.stack.append(token)
-        
+            return lastElement
+
+        if isinstance(element, dict):
+            # Should be a while here as other dicts are handled in if ladder
+            self.dot.add_node(element['cond'], **self.attr['COND'])
+            self.connectFrom(element['cond'], lastElement, firstEdgeName)
+
+            # Handle while body
+            lastElement = self.handle(element['body'], element['cond'], 'True')
+
+            # Connect back to condition
+            self.connectFrom(element['cond'], lastElement)
+
+            # While loop always exits with failed condition
+            return [element['cond']]
+
+
+        if isinstance(element, tuple):
+            # Must of if/else body, handle in loop
+            for e in element:
+                lastElement = self.handle(e, lastElement, firstEdgeName)
+                firstEdgeName = None
+
+            return lastElement
+
 
     def write(self, file):
         '''Write to DOT file'''
@@ -137,10 +238,19 @@ def main():
     '''The Main'''
 
     lexer = FlowLexer()
+    parser = FlowParser()
+
+    # print(list(lexer.tokenize("".join(fileinput.input()))))
+    # print(json.dumps(parser.parse(lexer.tokenize("".join(fileinput.input()))), indent=4))
+    builder = AstBuilder(parser.parse(lexer.tokenize("".join(fileinput.input()))))
+
+    '''
     builder = FlowBuilder(lexer.tokenize("".join(fileinput.input())))
+    '''
 
     builder.write('flowchart.dot')
     os.system('dot -T jpg -o flowchart.jpg flowchart.dot')
+
 
 if __name__ == '__main__':
     main()
