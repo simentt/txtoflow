d8cb6a1fd6e7ea6a2d719f1e5e6342a7ff7cfff0
Vijaya Krishna Kasula <krishna.vijay4444@gmail.com>
2019-06-16 11:42:36 +0530

    simplify setup

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="txtoflow",
-    version="0.1.0",
+    version="0.0.1",
     author="Krishna",
     author_email="krishna.vijay4444@gmail.com",
     description="Library to generate flowcharts from pseudo code",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/KrishKasula/txtoflow",
     packages=setuptools.find_packages(),
