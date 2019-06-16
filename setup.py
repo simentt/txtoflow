ca1a504aa9b95453198c770efefe7fe4ba7ad11c
Vijaya Krishna Kasula <krishna.vijay4444@gmail.com>
2019-06-16 12:10:47 +0530

    Version bump add platform and license and point to webpage isntead of github

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="txtoflow",
-    version="0.0.1",
+    version="0.1.0",
     author="Krishna",
     author_email="krishna.vijay4444@gmail.com",
     description="Library to generate flowcharts from pseudo code",
     long_description=open('README.md').read(),
+    platforms=['any'],
+    license='MIT License',
     long_description_content_type="text/markdown",
-    url="https://github.com/KrishKasula/txtoflow",
+    url="https://krishkasula.github.io/txtoflow",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
