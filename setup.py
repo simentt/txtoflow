dd0f1b64a59d289f76b3a2eeaf66b1910cc7b968
Vijaya Krishna Kasula <krishna.vijay4444@gmail.com>
2019-06-17 10:58:32 +0530

    add dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="txtoflow",
-    version="0.2.3",
+    version="0.2.6",
     author="Krishna",
     author_email="krishna.vijay4444@gmail.com",
     description="Library to generate flowcharts from pseudo code",
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://krishkasula.github.io/txtoflow",
     packages=setuptools.find_packages(),
+    install_requires=['sly', 'pygraphviz'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
